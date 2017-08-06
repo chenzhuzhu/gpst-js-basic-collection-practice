edbdf929ca41cc852e3022615705df3929a1d269
yuzhu <yuzhucyz@163.com>
2017-08-06 02:35:57 +0000

    section2-practice2 reconstruct

--- main/section-2/practice-2.js
+++ main/section-2/practice-2.js
@@ -1,29 +1,55 @@
 'use strict';
+function expand(collection){
+      let temp_result =[];
+      for(let each_item of collection){
+            if (each_item.length ===1){
+                 temp_result.push(each_item);
+            }else{
+                  let {key,count}=split(each_item);
+                  push(temp_result,key,count);
+            }
+      }
+      return temp_result
+}
+
+function split(item){
+      let splited_item = item.split('-');
+      return {key:splited_item[0],count:parseInt(splited_item[1],10)}
+}
+
+function push(result,key,count){
+      for(let i=0;i<count;i++){
+            result.push(key);
+      }
+}
+
+function summarize(collection){
+      let newarr=[];    
+      for(let item of collection){
+            let this_obj =find(item,newarr);
+            if(this_obj){
+                this_obj.count++;
+            }else{
+                newarr.push({key:item,count:1});
+            }
+      }
+      return newarr;
+      
+}
+
+function find(item,newarr){
+      for(let each_item of newarr){
+            if(item === each_item.key){
+                  return each_item;
+            }         
+      }
+      return false;
+}
 
 module.exports = function countSameElements(collection) {
-    
-    var new_arr=[];
-    for(var i=0;i<collection.length;){
-           if (collection[i].length>1) {
-        	   var this_value = parseInt(collection[i].replace(/[^0-9]/ig,"")); 
-        	   var this_key = collection[i].replace(/[^a-z]/ig,"");
-        	   new_arr.push({key:this_key,count:this_value});
-        	   collection.splice(i,1);
-	       }else{
-	           var this_count=0;   
-               for(var j=i;j<collection.length;j++){
-                   if (collection[i]==collection[j]){
-                        this_count+=1;
-                    }
-                }
-                new_arr.push({key:collection[i],count:this_count})
-                i+=this_count;        
-            }
-            
-                 
-	   }
-	   //console.log(new_arr);
-	   return new_arr;
+    let expand_collection = expand(collection);
+    return summarize(expand_collection);
+
    }
 
 
